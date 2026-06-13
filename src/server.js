4b9e48ff0c909179bbfeee76008ce12c1595568d
Jutsu78 <loshataraev@gmail.com>
2026-06-13 21:00:10 +0300

    Prisma client setup and manga series retrieval endpoint was added

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const prisma = require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,13 @@ app.get('/api/health', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// GET: Recieves all manga series in db
+app.get('/api/manga', async (req, res) => {
+    try {
+        const series = await prisma.series.findMany();
+        res.status(200).json({ status: 'ok', data: series });
+    } catch (error) {
+        console.error('Error during fetching manga series:', error);
+        res.status(500).json({ status: 'error', error: "Internal Server Error" });
+    }
 });
