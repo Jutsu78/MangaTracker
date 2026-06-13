a3dac1b94306e1b5792c1ddb4ea8866fa7c4b0cc
Jutsu78 <loshataraev@gmail.com>
2026-06-13 21:14:02 +0300

    endpoints for manga management were added

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-// checking server status
 app.get('/api/health', (req, res) => {
     res.status(200).json({
         status: 'ok',
@@ -23,4 +22,28 @@ app.get('/api/manga', async (req, res) => {
         console.error('Error during fetching manga series:', error);
         res.status(500).json({ status: 'error', error: "Internal Server Error" });
     }
+});
+
+// POST: Adding a new manga series to the db
+app.post('/api/manga', async (req, res) => {
+    try {
+        const { title, author, totalVolumes } = req.body;
+
+        const newSeries = await prisma.series.create({
+            data: {
+                title,
+                author,
+                totalVolumes
+            }
+        });
+
+        res.status(201).json({ status: 'ok', data: newSeries });
+    } catch (error) {
+        console.error('Error during adding manga series:', error);
+        res.status(500).json({ status: 'error', error: "Internal Server Error" });
+    }
+});
+
+    app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
